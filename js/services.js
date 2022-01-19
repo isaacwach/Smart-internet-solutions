d67910a58be48e902ab38e2160c6e05d031e6d74
isaacwach <isaacwach29@gmail.com>
2022-01-19 19:34:51 +0300

    validated phone number input

--- js/services.js
+++ js/services.js
@@ -1,15 +1,31 @@
 class packages {
-    constructor(daily, weekly, monthly, payment) {
+    constructor(daily, weekly, monthly) {
         this.daily = daily
         this.weekly = weekly
         this.monthly = monthly
-        this.payment = payment
-    }
-
-    payMethod(){
-        var payPeriod=(this.daily, this.weekly, this.monthly)
         
     }
+    printPackages() {
+        console.log(this.daily, this.weekly, this.monthly)
+    }
+   
+
+    // payMethod(){
+    //     if(isNaN(parseInt(this.daily))){
+    //         this.daily=0
+    //         return false
+    //     }
+    //     if(isNaN(parseInt(this.weekly))){
+    //         this.weekly=0
+    //         return false
+    //     }
+    //     if(isNaN(parseInt(this.monthly))){
+    //         this.monthly=0
+    //         return false
+    //     }
+    //     var totalPay=(parseInt(this.daily)+ parseInt(this.weekly)+ parseInt(this.monthly))
+
+    // }
     
 }
 
@@ -18,13 +34,55 @@ class packages {
 $(document).ready(function() {
     $("#btn2").click(function(){
         var dailyPay=parseInt($("#daily option:selected").val());
-        var weeklyPay=parseInt($("#wekly option:selected").val());
+        var weeklyPay=parseInt($("#weekly option:selected").val());
         var monthlyPay=parseInt($("#monthly option:selected").val());
-        var payment = parseInt($("#payment-option option:selected").val());
-        alert("Your total pay is "+dailyPay + " which you are required to  through " + payment + " within a week.")
-        alert("Your total pay is "+weeklyPay + " which you are required to  through " + payment + " within a week.")
-        alert("Your total pay is "+monthlyPay + " which you are required to  through " + payment + " within a week.")
+        var payment = $("#payment-option option:selected").val();
+
+       
+        // alert("Your total pay is "+weeklyPay + " which you are required to  through " + payment + " within a week.")
+        // alert("Your total pay is "+monthlyPay + " which you are required to  through " + payment + " within a week.")
         var finalPay= new packages(dailyPay, weeklyPay, monthlyPay)
         
+        if(isNaN(parseInt(this.daily))){
+            this.daily=0
+            return false
+        }
+        if(isNaN(parseInt(this.weekly))){
+            this.weekly=0
+            return false
+        }
+        if(isNaN(parseInt(this.monthly))){
+            this.monthly=0
+            return false
+        }
+
+        console.log(finalPay)
+        for (const key in finalPay) {
+
+            
+           finalPrice=`${finalPay[key]}`
+        console.log(finalPrice)
+        }
+        
+        
+        var totalPay=(parseInt(this.daily)+ parseInt(this.weekly)+ parseInt(this.monthly))
+        // console.log(totalPay)
     })
 })
+
+function phonenumber(){
+    var phoneNumber=document.getElementById("phone").value
+  var phoneno = /^\d{10}$/;
+  if(phoneNumber.match(phoneno)){
+        
+      return true;
+        }
+      else
+        {
+        alert("Please put a valid phone number");
+        return false;
+        }
+}
+
+
+
