bb353532f02ca47cc7e1b38633ecac6a3ffbc761
isaacwach <isaacwach29@gmail.com>
2022-01-19 22:53:12 +0300

    form validation

--- js/services.js
+++ js/services.js
@@ -71,31 +71,73 @@ $(document).ready(function () {
 
 })
 
-function phonenumber() {
-    var phoneNumber = document.getElementById("phone").value
-    var phoneno = /^\d{10}$/;
-    if (phoneNumber.match(phoneno)) {
+// function phonenumber() {
+//     var phoneNumber = document.getElementById("phone").value
+//     var phoneno = /^\d{10}$/;
+//     if (phoneNumber.match(phoneno)) {
 
-        return true;
-    } else {
+//         return true;
+//     } else {
+//         Swal.fire({
+//             icon: 'error',
+//             title: 'Error',
+//             text: 'Put a valid phone Number!',
+//             footer: '<a href="">Why do I have this issue?</a>'
+//           })
+//         return false;
+//     }
+// }
+
+// function ValidateEmail() {
+//     var inputtedEmail=document.getElementById("email").value
+//     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     if (inputtedEmail.match(mailformat)) {
+//         alert("Valid email address!");
+//         // document.form1.text1.focus();
+//         return true;
+//     } else {
+//         Swal.fire({
+//             icon: 'warning',
+//             title: 'Error',
+//             text: 'Put a valid email address!',
+//             footer: '<a href="">Why do I have this issue?</a>'
+//           })
+//         // document.form1.text1.focus();
+//         return false;
+//     }
+// }
+
+// function locationForm() {
+//     var subcounty= document.getElementById("sub-county").value
+//     var budget= document.getElementById("budget").value
+//     if(subcounty===""||budget=="") {
+//         Swal.fire({
+//             icon: 'error',
+//             title: 'Error',
+//             text: 'This field is required!',
+//           })
+//           return false
+//     }
+// }
+
+function validateForm() {
+    var phoneNumber = document.getElementById("phone").value
+    var phoneno = /^\+(?:[0-9] ?){6,14}[0-9]$/
+    var inputtedEmail=document.getElementById("email").value
+    var mailformat =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var subcounty= document.getElementById("sub-county").value
+    var budget= document.getElementById("budget").value
+    var address= document.getElementById("street-address").value
+    
+    if (phoneNumber.match(phoneno)||phoneNumber==""||phoneNumber==null){
         Swal.fire({
             icon: 'error',
             title: 'Error',
             text: 'Put a valid phone Number!',
             footer: '<a href="">Why do I have this issue?</a>'
           })
-        return false;
-    }
-}
-
-function ValidateEmail() {
-    var inputtedEmail=document.getElementById("email").value
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (inputtedEmail.match(mailformat)) {
-        alert("Valid email address!");
-        // document.form1.text1.focus();
-        return true;
-    } else {
+        return false
+    }else if (inputtedEmail.match(mailformat)||inputtedEmail==""||inputtedEmail==null){
         Swal.fire({
             icon: 'warning',
             title: 'Error',
@@ -104,5 +146,34 @@ function ValidateEmail() {
           })
         // document.form1.text1.focus();
         return false;
+    } else if(subcounty===""||subcounty==null){
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Subcounty field is required!',
+          })
+          return false
+    } else if(budget==""||budget==null){
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Budget field is required!',
+          })
+          return false
+    }else if(address==""||address==null){
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Address field is required!',
+          })
+          return false
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Response recorded',
+            text: 'Thank you for reaching us. We will get to you back shortly.',
+          })
+          return true
     }
+    
 }
