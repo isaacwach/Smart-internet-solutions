ac88d6a6591874cff794a8b8e17732bd05fb2491
davidkiama <davidkiama98@gmail.com>
2022-01-21 08:26:43 +0300

    Spaced the login page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-
- function validate(){
-
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    if(username=="admin"&& password == "user")
-    {
-        alert("Successful!");
-        return false;
-    }
-    else{
-        alert("Failed!")
-    }
- }
-
 const loginForm = document.querySelector(".login_form");
 
 loginForm.addEventListener("submit", (e) => {
